80f62dba5910b0c6b33148aae1dbbd8a87e8ea73
anji <anji@k.com>
2017-08-04 08:36:03 +0100

    comit test

---
+++ WebApiTest/WebApiTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTest
+{
+  public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("WebApi console test ");
+            Console.WriteLine("Enter any key to exit:");
+            Console.ReadLine();
+        }
+    }
+}
